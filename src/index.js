3861492b6714c7536ab3588c93b21fb8127849c6
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2020-04-28 11:23:56 -0500

    linter staged changes

--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
 import App from "./App";
 
 import "reset-css/reset.css";
 import "normalize-css/normalize.css";
-import "./index.css";
+import "./assets/main.css";
 
 /**********************************************************/
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
