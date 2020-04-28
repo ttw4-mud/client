73728dee5b9bf001e4614c5a13455b109bb70e06
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2020-04-28 18:02:21 -0500

    validation using yup/formik

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,14 +1,17 @@
 import React from "react";
 import axios from "axios";
 import { withFormik, Formik } from "formik";
-// import {Link} from 'react-router-dom';
 import * as Yup from "yup";
+
 const SignupSchema = Yup.object().shape({
   username: Yup.string()
     .max(50, "Username be less than 50 characters")
     .required("Username is Required"),
   email: Yup.string().email("Invalid email").required("Email required"),
-  password: Yup.string().required("password required"),
+  password: Yup.string()
+    .required("password required")
+    .min(8, "Password is too short - should be 8 chars minimum.")
+    .matches(/(?=.*[0-9])/, "Password must contain a number."),
 });
 const SignUp = () => (
   <div>
@@ -19,87 +22,104 @@ const SignUp = () => (
         password: "",
       }}
       validationSchema={SignupSchema}
-      onSubmit={(values) => {
-        console.log(values);
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          console.log("Logging in", values);
+          setSubmitting(false);
+        }, 500);
       }}
     >
-      {({ errors, touched }) => (
-        <div class="w-full max-w-xs">
-          <form
-            class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-            onSubmit={handleSubmit}
-          >
-            <div class="mb-4">
-              <label
-                class="block text-gray-700 text-sm font-bold mb-2"
-                for="username"
-              >
-                Username
-              </label>
-              <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="username"
-                type="text"
-                placeholder="Username"
-                className={errors.username && touched.username && "error"}
-              />
-              {errors.username && touched.username && (
-                <div className="input-feedback">{errors.username}</div>
-              )}
-            </div>
-            <div class="mb-4">
-              <label
-                class="block text-gray-700 text-sm font-bold mb-2"
-                for="email"
-              >
-                Email
-              </label>
-              <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="email"
-                type="text"
-                placeholder="Email"
-                className={errors.email && touched.email && "error"}
-              />
-              {errors.email && touched.email && (
-                <div className="input-feedback">{errors.email}</div>
-              )}
-            </div>
-            <div class="mb-6">
-              <label
-                class="block text-gray-700 text-sm font-bold mb-2"
-                for="password"
-              >
-                Password
-              </label>
-              <input
-                class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                id="password"
-                type="password"
-                placeholder="Password"
-                className={errors.password && touched.password && "error"}
-              />
-              {errors.password && touched.password && (
-                <div className="input-feedback">{errors.password}</div>
-              )}
-              <p class="text-red-500 text-xs italic">
-                Please choose a password.
-              </p>
-            </div>
-            <div class="flex items-center justify-between">
-              <button
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="button"
-              >
-                Sign Up
-              </button>
-            </div>
-          </form>
-          <p class="text-center text-gray-500 text-xs">
-            &copy;2020 Acme Corp. All rights reserved.
-          </p>
-        </div>
-      )}
+      {(props) => {
+        const {
+          values,
+          touched,
+          errors,
+          isSubmitting,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+        } = props;
+        return (
+          <div className="w-full max-w-xs">
+            <form
+              className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+              onSubmit={handleSubmit}
+            >
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="username"
+                >
+                  Username
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  name="username"
+                  id="username"
+                  type="text"
+                  placeholder="Enter Username"
+                  value={values.username}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.username && touched.username && (
+                  <div>{errors.username}</div>
+                )}
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  name="email"
+                  id="email"
+                  type="text"
+                  placeholder="Enter your email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.email && touched.email && <div>{errors.email}</div>}
+              </div>
+              <div className="mb-6">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <input
+                  className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  name="password"
+                  id="password"
+                  type="password"
+                  placeholder="Enter your password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.password && touched.password && (
+                  <div>{errors.password}</div>
+                )}
+                <p className="text-red-500 text-xs italic"></p>
+              </div>
+              <div className="flex items-center justify-between">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="submit"
+                  disabled={isSubmitting}
+                >
+                  Sign Up
+                </button>
+              </div>
+            </form>
+          </div>
+        );
+      }}
     </Formik>
   </div>
 );
@@ -120,9 +140,10 @@ const FormikLogin = withFormik({
     axios
       .post("url", params)
       .then((response) => {
-        localStorage.setItem("token", response.data.token);
-        localStorage.setItem("user", response.data.user.id);
-        props.history.push("/favorites");
+        console.log(response.data);
+        // localStorage.setItem("token", response.data.token);
+        // localStorage.setItem("user", response.data.user.id);
+        props.history.push("/game");
         resetForm();
       })
       .catch((error) => {
