0c789368d87252c72b077a231adb31c14efc51bf
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2020-04-29 12:22:33 -0500

    sets private route to game

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Route } from "react-router-dom";
 /**********************************************************/
+import PrivateRoute from "./auth/PrivateRoute";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import Game from "./components/Game";
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={SignIn} />
       <Route exact path="/signup" component={SignUp} />
+      <PrivateRoute component={Game} exact path="/game" />
     </div>
   );
 }
