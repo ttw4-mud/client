b0ed6156a84f7b87e8b91accca77d7dc043bcce1
Ariana Shackelford <ariana.shackelford@gmail.com>
2020-04-29 14:56:24 -0400

    Instructions component filled out

---
+++ src/components/Instructions/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Instructions = () => {
+  return (
+    <div>
+      Escape the dungeon! Use the arrow keys, or 'w' 'a' 's' 'd' to move. Good
+      luck traveler!
+    </div>
+  );
+};
+
+export default Instructions;
