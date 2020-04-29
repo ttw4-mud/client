7c24cbd5a34047c23fb17624132bb5c38d285769
Ariana Shackelford <ariana.shackelford@gmail.com>
2020-04-29 15:03:13 -0400

    Instructions component added to game page

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Instructions from "../Instructions/index";
 
 const Game = () => {
-  return <h1>You have been redirect to the game page....coming soon</h1>;
+  return (
+    <div>
+      <h1>You have been redirect to the game page....coming soon</h1>
+      <Instructions />
+    </div>
+  );
 };
 
 export default Game;
