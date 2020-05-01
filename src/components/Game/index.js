afad2bc86175c7d5747dc168192cfab7976fa6ea
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2020-05-01 09:23:52 -0500

    cleans up game page

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,63 +1,31 @@
-import React, { useState, useEffect } from "react";
-import axiosWithAuth from "../../auth/index";
+import React from "react";
 import Instructions from "../Instructions/index";
 import Character from "./Character";
-import Map from "../Map/index";
+import MapComponent from "../Map/index";
+
 import {
   MapWrapper,
   GamePage,
   StartButton,
   ButtonWrapper,
+  // SignOutButton
 } from "../CustomStyles/index";
 
-const Game = ({ status }) => {
-  const [state, setState] = useState();
-
-  useEffect(() => {
-    if (status) {
-      setState([...state, status]);
-    }
-  }, [state, status]);
-
-  // movement endpoint
-  const handleMovement = (direction) => {
-    axiosWithAuth()
-      .post(
-        "https://cors-anywhere.herokuapp.com/https://ttw4-mud-server--staging.herokuapp.com/api/adventure/move/",
-        direction
-      )
-      .then((response) => {
-        console.log("adventure response", response.data);
-      })
-      .catch((error) => {
-        alert(error.message);
-      });
-  };
-
-  //where the game starts endpoint
-
-  const initializeGame = (e) => {
-    e.persist();
-    axiosWithAuth()
-      .post(
-        "https://cors-anywhere.herokuapp.com/https://ttw4-mud-server--staging.herokuapp.com/api/adventure/start/"
-      )
-      .then((response) => {
-        console.log("initializeGame response", response.data);
-      })
-      .catch((error) => {
-        alert(error.message);
-      });
-  };
+const Game = () => {
+  // const SignOut = () => {
+  //   localStorage.clear('token');
+  //   // eslint-disable-next-line no-restricted-globals
+  //   history.push('/');
+  // };
 
   return (
     <GamePage>
       <Character />
       <MapWrapper>
-        <StartButton onClick={initializeGame}>Start</StartButton>
-        <Map />
+        <StartButton>Start</StartButton>
+        <MapComponent />
         <Instructions />
-        <ButtonWrapper onClick={handleMovement}></ButtonWrapper>
+        <ButtonWrapper></ButtonWrapper>
       </MapWrapper>
     </GamePage>
   );
