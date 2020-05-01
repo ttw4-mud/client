59fde9f146f9a3f1868cb5e7e88f2506a78f8fba
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2020-05-01 09:23:00 -0500

    adds baseUrl for shorter endpoints

--- src/auth/index.js
+++ src/auth/index.js
@@ -4,6 +4,8 @@ const axiosWithAuth = () => {
   const token = localStorage.getItem("key");
 
   return axios.create({
+    baseURL:
+      "https://cors-anywhere.herokuapp.com/https://ttw4-mud-server--staging.herokuapp.com/api",
     headers: {
       "Content-Type": "application/json",
       Authorization: token,
